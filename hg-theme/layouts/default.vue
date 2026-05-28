f91ebf0be4fc740a272d26f98a7b43423af50481
drewgilbert-lab <drew.gilbert@hginsights.com>
2026-05-28 11:19:21 -0700

    Replace logo image with updated version

--- hg-theme/layouts/default.vue
+++ hg-theme/layouts/default.vue
@@ -3,7 +3,7 @@
     <slot />
     
     <div class="absolute bottom-4 left-12 right-12 flex justify-between items-end text-[9px] text-[#818282]">
-      <img src="/logo.svg" class="w-[1.55in] object-contain" alt="HG Insights" />
+      <img src="/HGInsights-Logo-blue.svg" class="w-[1.55in] object-contain" alt="HG Insights" />
       
       <div class="absolute left-1/2 transform -translate-x-1/2 bottom-0">
         ©2026 HG Insights proprietary and confidential.
