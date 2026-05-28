f0398bdce43132fe2a859fa7e674765afe36df54
drewgilbert-lab <drew.gilbert@hginsights.com>
2026-05-28 11:09:06 -0700

    Add default layout template in default.vue

---
+++ hg-theme/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="slidev-layout default bg-white text-hg-dark h-full w-full p-12">
+    <slot />
+    
+    <div class="absolute bottom-4 left-12 right-12 flex justify-between items-end text-[9px] text-[#818282]">
+      <img src="/logo.svg" class="w-[1.55in] object-contain" alt="HG Insights" />
+      
+      <div class="absolute left-1/2 transform -translate-x-1/2 bottom-0">
+        ©2026 HG Insights proprietary and confidential.
+      </div>
+      
+      <div class="mb-0.5"><SlideCurrentNo /></div>
+    </div>
+  </div>
+</template>
