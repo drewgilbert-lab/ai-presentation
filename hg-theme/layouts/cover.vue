e28f826e282cd1f1310320ee7a46767435c9aaf0
drewgilbert-lab <drew.gilbert@hginsights.com>
2026-05-28 11:18:47 -0700

    Update cover.vue

--- hg-theme/layouts/cover.vue
+++ hg-theme/layouts/cover.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="slidev-layout cover bg-hg-navy text-white h-full w-full p-12 flex flex-col justify-center">
-    <img src="/logo.svg" class="absolute top-8 left-8 w-[2.5in] object-contain brightness-0 invert" alt="HG Insights" />
+    <img src="/HGInsights-Logo-white.svg" class="absolute top-8 left-8 w-[2.5in] object-contain" alt="HG Insights" />
     
     <div class="mt-8">
       <slot />
