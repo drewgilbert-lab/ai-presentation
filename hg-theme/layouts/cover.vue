67145bd17e7eed0a18cbfdc0bd5a0627c7ecb640
drewgilbert-lab <drew.gilbert@hginsights.com>
2026-05-28 11:09:48 -0700

    Add cover layout component in cover.vue

---
+++ hg-theme/layouts/cover.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="slidev-layout cover bg-hg-navy text-white h-full w-full p-12 flex flex-col justify-center">
+    <img src="/logo.svg" class="absolute top-8 left-8 w-[2.5in] object-contain brightness-0 invert" alt="HG Insights" />
+    
+    <div class="mt-8">
+      <slot />
+    </div>
+    
+    <div class="absolute bottom-6 right-8 text-[14px]">CONFIDENTIAL</div>
+  </div>
+</template>
